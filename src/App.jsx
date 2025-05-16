f7c8f3501b769643dc762c70400a8172378ad09a
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2025-05-16 12:22:38 -0500

    Updated and added assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Home from "./pages/Home";
+import Classes from "./pages/Classes";
+import Instructors from "./pages/Instructors";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ScrollToTopButton from "./components/ScrollToTopButton";
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/classes" element={<Classes />} />
+        <Route path="/instructors" element={<Instructors />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-tan">Harmony Yoga is back ✨</h1>
-    </div>
-  )
-}
+    <Router>
+      <div className="bg-ivory min-h-screen text-dark">
+        <Navbar />
+        <AnimatedRoutes />
+        <Footer />
+        <ScrollToTopButton />
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
