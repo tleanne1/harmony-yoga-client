cf528200564d777abd14b4c54a5fbc966bd97567
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2025-05-16 11:49:44 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-tan">Harmony Yoga is back ✨</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#0A0908",
+        slate: "#22333B",
+        ivory: "#F2F4F3",
+        tan: "#A9927D",
+        brown: "#5E503F",
+      },
+    },
+  },
+  plugins: [],
+}
